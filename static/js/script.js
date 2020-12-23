37666277ad42cc026521ef11944dd28728fb4264
karzuin <karzuin@yahoo.com>
2020-12-23 15:27:19 +0000

    Add category dropdown menu, text field, checkbox to add joke template

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
     $(".sidenav").sidenav({edge:"right"});
+     $('select').formSelect();
+    $('#textarea1').val();
+  M.textareaAutoResize($('#textarea1'));
   });
