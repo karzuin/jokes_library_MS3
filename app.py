a7cdd8b19111f02bffe2503f325fbd247a223f5e
karzuin <karzuin@yahoo.com>
2021-01-15 16:29:56 +0000

    Disabled debugger

--- app.py
+++ app.py
@@ -493,4 +493,4 @@ def yo_jokes():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
